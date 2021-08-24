a344aeee3af635c4f25c58826eddfb2d379a1781
PhilAtKitronik <37149430+PhilAtKitronik@users.noreply.github.com>
2021-08-24 09:31:44 +0100

    Update Basic3SensorLineFollow.py
    updated comments and camelCase

--- Basic3SensorLineFollow.py
+++ Basic3SensorLineFollow.py
@@ -28,27 +28,27 @@ buggy.show()
 
 
 while True:
-    LeftVal = buggy.getRawLFValue("l")
-    RightVal = buggy.getRawLFValue("r")
-    CenterVal = buggy.getRawLFValue("c")
+    leftVal = buggy.getRawLFValue("l")
+    rightVal = buggy.getRawLFValue("r")
+    centerVal = buggy.getRawLFValue("c")
     #black line means centre should be high value, so if its not w eare off the line.
-    if(CenterVal < 30000):
+    if(centerVal < 30000):
         #figure out which side we went
-        if(LeftVal < (RightVal)):
+        if(leftVal < (rightVal)): #turn right
             buggy.setLED(0,buggy.YELLOW)
             buggy.setLED(1,buggy.RED)
             buggy.setLED(2,buggy.RED)
             buggy.setLED(3,buggy.YELLOW)
             buggy.motorOn("l","f",75)
             buggy.motorOn("r","r",75)
-        elif (RightVal <  (LeftVal)):
+        elif (rightVal <  (leftVal)): #turn left
             buggy.setLED(0,buggy.RED)
             buggy.setLED(1,buggy.YELLOW)
             buggy.setLED(2,buggy.YELLOW)
             buggy.setLED(3,buggy.RED)
             buggy.motorOn("l","r",75)
             buggy.motorOn("r","f",75)
-    else:
+    else: #buggy should drive forward
         buggy.setLED(0,buggy.GREEN)
         buggy.setLED(1,buggy.GREEN)
         buggy.setLED(2,buggy.GREEN)
