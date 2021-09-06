c59e118975fb9bee77af630741624736808a24d5
DaveAtKitronik <dave.sanderson@kitronik.co.uk>
2021-09-06 11:34:55 +0100

    initial add

---
+++ Tutorial_Code/UltrasonicPrintRange.py
@@ -0,0 +1,9 @@
+from PicoAutonomousRobotics import KitronikPicoRobotBuggy
+from time import sleep
+
+buggy = KitronikPicoRobotBuggy()
+while True:
+   frontDistance = buggy.getDistance("f")
+   print(“Front Distance:”, frontDistance)
+   sleep(1)
+    
